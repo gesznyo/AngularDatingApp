178bd84905ec99af9c3edd1c70702f436517a41c
gesznyo <janos.koczka@gmail.com>
2021-03-10 12:44:45 +0100

    End of section 7

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.Errors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace API.Middleware
+{
+  public class ExceptionMiddleware
+  {
+    private readonly IHostEnvironment _env;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+    {
+      _next = next;
+      _logger = logger;
+      _env = env;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+      try
+      {
+          await _next(context);
+      }
+      catch (Exception e)
+      {
+          _logger.LogError(e, e.Message);
+          context.Response.ContentType = "application/json";
+          context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+          var response = _env.IsDevelopment()
+          ? new ApiException(context.Response.StatusCode, e.Message, e.StackTrace?.ToString())
+          : new ApiException(context.Response.StatusCode, "Internal Server Error.");
+
+          var option = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+          var jsonResponse = JsonSerializer.Serialize(response, option);
+
+          await context.Response.WriteAsync(jsonResponse);
+      }
+    }
+
+  }
+}
---
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router, private toastr: ToastrService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((errorResponse: HttpErrorResponse) => {
+        if (errorResponse) {
+          switch (errorResponse.status) {
+            case 400:
+              if (errorResponse.error.errors) {
+                const modelStateErrors = [];
+                for (const key in errorResponse.error.errors) {
+                  if (
+                    Object.prototype.hasOwnProperty.call(
+                      errorResponse.error.errors,
+                      key
+                    )
+                  ) {
+                    const element = errorResponse.error.errors[key];
+                    modelStateErrors.push(element);
+                  }
+                }
+                throw modelStateErrors.flat();
+              } else {
+                this.toastr.error(
+                  errorResponse.error,
+                  errorResponse.status.toString()
+                );
+              }
+              break;
+
+            case 401:
+              this.toastr.error(
+                'Unauthorized',
+                errorResponse.status.toString()
+              );
+              break;
+
+            case 404:
+              this.router.navigateByUrl('/not-found');
+              break;
+
+            case 500:
+              const navigationExtras: NavigationExtras = {
+                state: { error: errorResponse.error },
+              };
+              this.router.navigateByUrl('/server-error', navigationExtras);
+              break;
+
+            default:
+              this.toastr.error('Something unexpected went wrong.');
+              console.log(errorResponse);
+              break;
+          }
+        }
+
+        return throwError(errorResponse);
+      })
+    );
+  }
+}
